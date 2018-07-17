262d5588c48bfa4a35cd2dfad392090f8bd2d77f
ITX351 <itx351@126.com>
2018-07-17 20:20:50 +0800

    补充修改

--- src/App.js
+++ src/App.js
@@ -30,32 +30,44 @@ class App extends Component {
 
     let red = [1, 2, 3, 4, 5, 6, 7, 8, 9];
     let blue = red.slice();
-    let playerList = playerNames.split("\n");
+    let playerNameList = playerNames.trim().split("\n");
+
+    for (let i = 0; i < playerNameList.length; i++) {
+      let playerName = playerNameList[i];
+      while (["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"].indexOf(playerName.slice(0, 1)) !== -1) {
+        playerName = playerName.slice(1, playerName.length);
+      }
+      playerNameList[i] = playerName;
+    }
+    if (playerNameList[playerNameList.length - 1].startsWith("随机")) {
+      playerNameList = playerNameList.slice(0, playerNameList.length - 1);
+    }
 
     if (shufflePlayerList) {
-      App.shuffleArray(playerList);
+      App.shuffleArray(playerNameList);
     }
     App.shuffleArray(red);
     App.shuffleArray(blue);
 
-    let aver = Math.floor(playerList.length / 2);
+    let aver = Math.floor(playerNameList.length / 2);
     let finalList = [];
 
     for (let i = 0; i < aver; i++) {
       finalList.push("红" + red[i].toString());
       finalList.push("蓝" + blue[i].toString());
     }
-    if (playerList.length % 2 === 1) {
+    if (playerNameList.length % 2 === 1) {
       finalList.push("审判者");
     }
 
     App.shuffleArray(finalList);
-    let result = "";
+    let newPlayerNames = "";
+    let secrets = "";
     for (let i = 0; i < finalList.length; i++) {
       let thisStr = finalList[i];
       let identityName = "";
       if (thisStr !== "审判者") {
-        identityName = identityNameList[parseInt(thisStr.slice(1, 2))];
+        identityName = identityNameList[parseInt(thisStr.slice(1, 2))] + " ";
       }
 
       let nextStr = finalList[(i + 1) % finalList.length];
@@ -68,9 +80,13 @@ class App extends Component {
         nextColor = Math.random() > 0.5 ? "红" : "蓝";
       }
 
-      result += (i + 1).toString() + " " + playerList[i] + " " + finalList[i] + " " + identityName + " 下家" + nextColor + "\n";
+      newPlayerNames += (i + 1).toString() + playerNameList[i] + "\n";
+      secrets += (i + 1).toString() + playerNameList[i] + " " + finalList[i] + identityName + "下家" + nextColor + "\n";
     }
-    this.setState({result: result});
+
+    let originKnifeNumber = Math.ceil(Math.random() * playerNameList.length);
+    newPlayerNames += "随机" + originKnifeNumber.toString() + "提刀";
+    this.setState({playerNames: newPlayerNames, result: secrets});
   }
 
   render() {
